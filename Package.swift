ab8e7da599e969a46c0cf73acf8fe5c3220d6774
Ignas Valotka <ignas.valotka@trafi.com>
2020-04-24 11:37:07 +0300

    Fix names

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "RatingSlider",
+    name: "StoryFlow",
     platforms: [
         .iOS(.v10),
     ], products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "RatingSlider",
-            targets: ["RatingSlider"]),
+            name: "StoryFlow",
+            targets: ["StoryFlow"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "RatingSlider",
+            name: "StoryFlow",
             dependencies: []),
     ]
 )
