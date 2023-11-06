f5de6597719b84de7c00fdcffa155edbb5da3781
klaidas-trafi <115455720+klaidas-trafi@users.noreply.github.com>
2023-11-06 15:22:29 +0200

    make produceDirect work in unwind scenarios (#21)

--- Sources/StoryFlow/Flow/ImplicitFlow/ImplicitFlow.swift
+++ Sources/StoryFlow/Flow/ImplicitFlow/ImplicitFlow.swift
@@ -66,7 +66,18 @@ extension Flow {
                 return
             }
             
-            // MARK: Direct
+            // MARK: Direct Unwind
+            
+            if let to = from.unwindVc(for: Swift.type(of: value)) {
+                to.handleUpdate(value, type)
+                let transition = TransitionInfo(from: from, producedType: output.type, receivedType: type, to: to, isUnwind: true)
+                if CustomTransition.attempt(transition) == false {
+                    Transition.unwind().go(from, to)
+                }
+                return
+            }
+            
+            // MARK: Direct Input
             
             if let to = Flow<Any>.destination(for: (value, Swift.type(of: value))) {
                 let transition = TransitionInfo(from: from, producedType: Swift.type(of: value), receivedType: type, to: to, isUnwind: false)
